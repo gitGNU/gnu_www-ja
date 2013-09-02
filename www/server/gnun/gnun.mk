68b1a2af8df7e2d078f15d945e8a1926ad805520
NIIBE Yutaka <gniibe@fsij.org>
2013-09-02 09:18:37 +0900

    Sync with CVS

--- www/server/gnun/gnun.mk
+++ www/server/gnun/gnun.mk
@@ -504,6 +504,7 @@ philosophy := 	15-years-of-free-software \
 		pragmatic \
 		privacyaction \
 		programs-must-not-limit-freedom-to-run \
+		proprietary \
 		proprietary-back-doors \
 		proprietary-sabotage \
 		proprietary-surveillance \
